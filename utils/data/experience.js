502bdddbe4ef70681b2aa1194a75f2b2cfd5138c
mallouk2020 <elyassmallouk@gmail.com>
2025-03-11 23:31:10 +0000

    v6.2

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -30,10 +30,16 @@ export const experiences = [
   //   duration: "(Jan 2022 - Present)"
   // },
   {
-    id: 2,
+    id: 1,
     title: "Full-Stack Developer",
     company: " (Freelance)",
     duration: "(Jun 2023 - Jan 2024)"
+  }, 
+  {
+    id: 2,
+    title: "UI/UX Designer for Mobile Apps",
+    company: "Freelance",
+    duration: "(Jan 2024 - Present)"
   },
   {
     id: 3,
@@ -41,10 +47,5 @@ export const experiences = [
     company: "Building Projects and Coding Independently",
     duration: "(Jan 2020 - Present)"
   },
-  {
-    id: 4,
-    title: "UI/UX Designer for Mobile Apps",
-    company: "Freelance",
-    duration: "(Jan 2024 - Present)"
-  }
+ 
 ];
